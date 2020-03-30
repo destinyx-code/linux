1bf6d4a27b3d4ce6449501e84e3af5c3dcc5954f
Ryder <ryderstanley2007@gmail.com>
2020-03-30 15:35:49 -0600

    Update dual_run.c

--- kernel/dual_run.c
+++ kernel/dual_run.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+	main() {
+		str os;
+		str windows = "Windows";
+		str macos = "MacOs";
+		str gnulinux = "Gnu/Linux";
+		str none;
+			printf("What Os would you like to run with Linux?");
+				scanf("%d", &os);
+					if (os == windows) {
+						
+					}
+	}
+return 0;
